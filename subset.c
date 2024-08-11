d3b57ecc49ff226d552ce1ac8076d436cfe3f9de
alonemask96 <phytonlab96@gmail.com>
2024-08-12 00:27:44 +0530

    subset.c

--- subset.c
+++ subset.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-    int flag, count;
+    int count;
     int x[100], w[100], d, n;
 
     void sum(int s, int k, int r) 
@@ -9,10 +9,10 @@
             if (s + w[k] == d) 
         {
              printf("\nsubset :%d", ++count);
-             flag = 1;
-             for (int i = 0; i <= k; i++)
+    
+             for (int i = 0; i<= k; i++)
                  if (x[i] == 1)
-                 printf("%d ", w[i]);
+                 printf("%d",w[i]);
             } 
         else if (s + w[k] + w[k + 1] <= d)
             sum(s + w[k], k + 1, r - w[k]);
@@ -26,7 +26,7 @@
     int main()
     {
       int r = 0;
-          flag = 0;
+    
           printf("enter the total no of elements:");
           scanf("%d", &n);
           for (int i = 0; i < n; i++)
@@ -37,14 +37,10 @@
           x[i] = 0;
           for (int i = 0; i < n; i++)
            r += w[i];
-          if (r < d) 
+          if (r > d) 
           {
-          printf("no subset is possible\n");
-          flag = 1;
+              sum(0, 0, r);
           } 
           else
-          sum(0, 0, r);
-          if (flag == 0)
-           printf("no more subset is possible\n");
-          return 0;
-       }
+          printf("no subset is possible\n");
+    }
