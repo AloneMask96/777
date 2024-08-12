3ff1db30d55ac1759448d1cf118b46e39af2ba37
alonemask96 <phytonlab96@gmail.com>
2024-08-12 19:01:56 +0530

    quick.c

--- quick.c
+++ quick.c
@@ -37,75 +37,45 @@ quicksort(a,s+1,r);
 }
 int main()
 {
-int temp,min,j,i,n,a[100000],choice;
-clock_t t;
-printf("enter the numbers of elements");
-scanf("%d",&n);
-printf("1.Read from file 2.Random numbers");
- scanf("%d", &choice);
-switch(choice)
-{
-case 1:
-printf("File numbers\n");
-FILE*file=fopen("data.txt","r");
-int i=0;
-while(! feof(file) && i<n)
-{
-fscanf(file, "%d",&a[i]);
-printf("%d\n",a[i]);
-i++;
+    int n, a[100000], choice;
+    clock_t t;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("1. Read from file\n2. Random numbers\n");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+    {
+        FILE* file = fopen("data.txt", "r");
+        if (file == NULL) 
+        {
+            printf("Error opening file.\n");
+            return 1;
+        }
+        for (int i = 0; i < n && fscanf(file, "%d", &a[i]) == 1; i++)
+            printf("%d\n", a[i]);
+        fclose(file);
+    }
+    else
+    {
+        printf("Random numbers:\n");
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = rand() % 1000;
+            printf("%d\n", a[i]);
+        }
+    }
+
+    t = clock();
+    quicksort(a, 0, n - 1);
+    t = clock() - t;
+
+    printf("Sorted numbers:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d\n", a[i]);
+
+    printf("Sort function took %f seconds to execute.\n", ((double)t) / CLOCKS_PER_SEC);
+    return 0;
 }
-fclose(file);
-break;
-case2:printf("Random number generator");
-for(i=0;i<n;i++)
-{
-a[i]=rand()%1000;
-printf("%d\n", a[i]);
-}
-break;
-}
-t=clock();
-quicksort(a,0,n-1);
-t=clock()-t;
-double time_taken=((double)t)/CLOCKS_PER_SEC;
-printf("entered numbers are after sorting");
-for(i=0;i<n;i++)
-printf("%d\n",a[i]);
-printf("sort function took %f seconds to execute \n",time_taken);
-return 0;
-}
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
